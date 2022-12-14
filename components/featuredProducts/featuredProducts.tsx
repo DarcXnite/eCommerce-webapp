8f37311c6552089b6f7c527036ba1b4a97df34df
DarcXnite <anhri89@yahoo.com>
2022-12-14 16:22:25 -0600

    added boilerplate modal

--- components/featuredProducts/featuredProducts.tsx
+++ components/featuredProducts/featuredProducts.tsx
@@ -5,7 +5,7 @@ export default function FeaturedProducts() {
   return (
     <>
       <header className='text-center bg-aro-green'>FEATURED PRODUCTS</header>
-      <div className='grid sm:grid-cols-2 xl:grid-cols-3 content-around justify-items-center w-3/4 xl:w-2/3 mx-auto'>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 content-around justify-items-center w-3/4 xl:w-2/3 mx-auto'>
         <Link href='#' className='fItem'></Link>
         <Link href='#' className='fItem'></Link>
         <Link href='#' className='fItem'></Link>
