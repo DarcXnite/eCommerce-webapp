f509fa5812b80539836daba9ceb64f5aded35737
ASelviyan <angelikaselviyan5@gmail.com>
2022-12-15 19:20:19 -0700

    each product

--- components/products/cards.tsx
+++ components/products/cards.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Cards() {
   return (
-    <div className='h-48 rounded-lg border-slate-300 border-2 w-52 p-6 m-16'>
-     
-        <h3 className='text-center'>Name</h3>
-        <p>price</p>
-    </div>
+    <>
+    <Link href=''>
+      <div className='h-64 rounded-lg border-slate-300 border-2 w-52 p-1 m-16'>
+          
+            <Image className='mt-7 mb-4 h-auto' src='/../public/assets/dagger.jpg' alt='dagger picture' height={480} width={300}/>
+          
+          <h3 className='text-center'>Zweihander</h3>
+          <p className='text-center'>$50.00</p>
+      </div>
+    
+    </Link>
+    
+    </>
   )
 }
--- components/products/categories.tsx
+++ components/products/categories.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Chevron from './chevron'
+import Link from 'next/link'
 
 export default function Categories() {
+
+    function sortType() {
+        //add a parameter of what the use clicked on 
+        //filter through the data that was given 
+        //look for the type of weapon that the use is looking for 
+        //then add that list to a useState and then map through it 
+        //displaying it on the page 
+    }
+    
   return (
-    <div className='h-96 rounded-lg border-aro-tan-dark border-2 w-52 p-6 m-16 '>
+    <div className=' h-96 rounded-lg  border-aro-tan-dark border-2 w-52 p-6 m-16 '>
         <h2 className='text-3xl font-bold mx-auto'>Categories</h2>
         <ul className='m-4 w-32'>
             <li className='pb-2 flex'>
+                {/* <button onClick={sort}> */}
                 <Chevron  />
-                Axes
+                    Axes
+                {/* </button> */}
+                    
             </li>
 
             <li className='pb-2 flex'> 
